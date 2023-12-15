a14de88cc1264eab29e69d14847720b1145a51f9
Chaluka Rathnayake <chaluka.rathnayaka@gmail.com>
2023-12-15 03:56:38 +0100

    Android Pages created

---
+++ app/src/main/java/com/example/iotautomatescenemanagementprojectht2023/VacationMode.java
@@ -0,0 +1,14 @@
+package com.example.iotautomatescenemanagementprojectht2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VacationMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_mode);
+    }
+}
